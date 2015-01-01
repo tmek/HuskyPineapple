c3559b69808457138e28e439ff26b3bbeb3a631c
tmek <Thomas.Kadlec@gmail.com>
2015-01-01 03:15:38 -0500

    - added artifacts/ to .gitignore, - added requirejs package Less processing: - added grunt-contrib-less package - setup grunt less task options - have grunt load less task - setup a taskrunner BeforeBuild binding to run the less task - added a foo.less source file - referneced foofoo.css result in _layout.cshtml NOTE: foofoo.css is a baked asset and therefore wouldn't normally be checked in.   I'm leaving it in for now until I decide how to deal with wwwroot and baked vs source items.

--- WebApp/gruntfile.js
+++ WebApp/gruntfile.js
@@ -1,4 +1,5 @@
-﻿// This file in the main entry point for defining grunt tasks and using grunt plugins.
+/// <binding BeforeBuild='less' />
+// This file in the main entry point for defining grunt tasks and using grunt plugins.
 // Click here to learn more. http://go.microsoft.com/fwlink/?LinkID=513275&clcid=0x409
 
 module.exports = function (grunt) {
@@ -11,11 +12,23 @@ module.exports = function (grunt) {
                     cleanTargetDir: false
                 }
             }
+        },
+        less: {
+            process: {
+                options: {
+                    paths: ["Content"],
+                },
+                files: { "wwwroot/css/foofoo.css": "content/foo.less" }
+            }
         }
     });
 
     // This command registers the default task which will install bower packages into wwwroot/lib
     grunt.registerTask("default", ["bower:install"]);
+
+
+
+    grunt.loadNpmTasks("grunt-contrib-less");
 
     // The following line loads the grunt plugins.
     // This line needs to be at the end of this this file.
